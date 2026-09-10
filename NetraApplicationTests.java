a06db6f15d6ffa421e0f8207a1c0ceda80fbdbeb
Sujal Sachin Aparna Akula <akulasujal2005@gmail.com>
2026-09-10 11:20:01 +0530

    Add files via upload

---
+++ NetraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sih.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
